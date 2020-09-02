ca15abc93aee744629a5ce24e8a6cc7e4181a6a1
n3k0m4 <achraf.el-masdouri@imt-atlantique.net>
2020-09-02 02:31:27 +0200

    working

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const helmet = require("helmet");
 const monk = require("monk");
 const morgan = require("morgan");
 const cors = require("cors");
+const yup = require('yup')
+import {nanoid} from 'nanoid';
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +13,11 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("./public"));
+
+const schema = yup.object().shape({
+  alias : yup.string().trim().matches(/[\w\-_]/i),
+  url : yup.string().trim().url().required()
+})
 /** 
 app.get("/", (req, res) => {
   //res.json("HOLA! This is a shortener");
@@ -20,9 +27,35 @@ app.get("/:id", (req, res) => {
    //For going to a URL 
 });
 **/
-app.post("/url", (req, res) => {
+app.post("/url", async (req, res, next) => {
   //For creating to a URL
+  const {alias, url} = req.body;
+  try {
+    await schema.validate({alias, url});
+    if (!alias){
+      alias = nanoid(7);
+    }
+    alias = alias.toLowerCase();
+    res.json({
+      'alias': alias,
+      'url': url
+    })
+  } catch (error) {
+    next(error);
+  }
 });
+app.use((error, req, res , next)=> {
+  if (error.status){
+    res.status(error.status)
+  }
+  else {
+    res.status(500);
+  }
+  res.json({
+    message : error.message,
+    stack: process.env.NODE_ENV === 'production' ? 'naah' : error.stack;
+  })
+})
 
 const port = process.env.PORT || 1337;
 
